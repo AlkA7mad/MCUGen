49faa53d47bf6d0bb48e7bfa1404e1d0553904ec
Ahmad <ahmad.alkwadri@yahoo.com>
2026-05-11 16:56:22 +0200

    Frontend: Create API service function to fetch available boards

---
+++ Frontend/src/api/boardAPI.ts
@@ -0,0 +1,22 @@
+
+interface Board {
+    id: string;
+    name: string;
+    mcu: string;
+    package: string;
+}
+
+export async function getBoards(): Promise<Board[]> {
+    
+    const response = await fetch("http://localhost:5131/boards");
+
+    if (response.status >= 500) {
+        throw new Error(`Server error: ${response.status}`);
+    }
+
+    const data: Board[] = await response.json();
+
+    console.log(data);
+
+    return data;
+}
